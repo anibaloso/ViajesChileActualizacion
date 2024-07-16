9a9317383e529c5b14c8046a0dfb5aecd5e5aaac
anibal <anibaloso@live.com>
2024-07-15 21:17:07 -0400

    proyecto terminado

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
+
+function enviar(){
+    alert("¡Tu mensaje ha sido enviado con exito!");
+}
